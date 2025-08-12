ee796e257cf0378571504dfb40e2245f86f5dd87
fabricio magoga <falolmagoga@gmail.com>
2025-08-12 15:14:35 -0300

    config deploy vercel

--- backend/server.js
+++ backend/server.js
@@ -92,4 +92,5 @@ app.get("/:shortUrl", async (req, res, next) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => console.log(`Server is running on port 3000`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
